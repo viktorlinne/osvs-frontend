5bd8abc48d0467de3f8737a3999006e50b3ab59e
Viktor Linné <viktor.linne@gmail.com>
2026-03-29 11:16:09 +0200

    chore: standardize last occurances of tailwinds green-600 to custom primary-600

--- src/components/layout/Banner.tsx
+++ src/components/layout/Banner.tsx
@@ -1,11 +1,11 @@
 export const Banner = () => {
   return (
-    <div className="w-full flex flex-col bg-gray-100 py-2 px-4 text-center text-sm text-green-600">
+    <div className="w-full flex flex-col bg-gray-100 py-2 px-4 text-center text-sm text-primary-600">
       <img src="https://kmxmlfhkojdbuoktavul.supabase.co/storage/v1/object/public/static/banner.png"
         alt="Ordenssamfundet VS Logo"
         className="mx-auto object-contain"
       />
-      <h1 className="text-xl font-bold text-green-600">ORDENSAMFUNDET VS</h1>
+      <h1 className="text-xl font-bold text-primary-600">ORDENSAMFUNDET VS</h1>
     </div>
   );
 };
